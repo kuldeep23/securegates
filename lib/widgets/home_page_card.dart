d8e70a20e3934107958b76e99c4afa22814a7f5b
yash <yashlathar07@gmail.com>
2023-06-26 16:45:30 +0530

    Featues card ui done

--- lib/widgets/home_page_card.dart
+++ lib/widgets/home_page_card.dart
@@ -15,34 +15,39 @@ class HomePageCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(5),
       margin: const EdgeInsets.all(3),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-              color: Colors.grey.shade600,
-              spreadRadius: 0.1,
-              blurRadius: 15,
-              offset: const Offset(0, 7))
-        ],
-      ),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Image(
-            image: image,
-            fit: BoxFit.fill,
-            height: 60,
-            width: 60,
+          Container(
+            height: 85,
+            width: 85,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey.shade600,
+                    spreadRadius: 0.1,
+                    blurRadius: 15,
+                    offset: const Offset(0, 7))
+              ],
+            ),
+            child: Image(
+              image: image,
+              fit: BoxFit.fill,
+            ),
           ),
           const SizedBox(
             height: 10,
           ),
           Text(
             featureText,
-            style: GoogleFonts.montserrat(fontSize: 11),
+            style: GoogleFonts.montserrat(
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+            ),
           ),
         ],
       ),
