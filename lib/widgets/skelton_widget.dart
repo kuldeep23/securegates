37caf1f6f4ecaa0a6964d8e5acb310f8749b9368
yash <yashlathar07@gmail.com>
2023-07-04 11:05:36 +0530

    shimmer done with typewriter animation

--- lib/widgets/skelton_widget.dart
+++ lib/widgets/skelton_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class Skeleton extends StatelessWidget {
   const Skeleton({Key? key, this.height, this.width}) : super(key: key);
@@ -18,6 +19,28 @@ class Skeleton extends StatelessWidget {
   }
 }
 
+class Shimming extends StatelessWidget {
+  const Shimming({super.key, this.height = 20, this.width = 20});
+
+  final double? height, width;
+
+  @override
+  Widget build(BuildContext context) {
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[400]!,
+      highlightColor: Colors.grey[600]!,
+      child: Container(
+        height: height,
+        width: width,
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: Colors.black.withOpacity(0.2),
+            borderRadius: const BorderRadius.all(Radius.circular(10))),
+      ),
+    );
+  }
+}
+
 class CircleSkeleton extends StatelessWidget {
   const CircleSkeleton({Key? key, this.size = 24}) : super(key: key);
 
@@ -25,12 +48,16 @@ class CircleSkeleton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: size,
-      width: size,
-      decoration: BoxDecoration(
-        color: Theme.of(context).primaryColor.withOpacity(0.04),
-        shape: BoxShape.circle,
+    return Shimmer.fromColors(
+      baseColor: Colors.grey[400]!,
+      highlightColor: Colors.grey[600]!,
+      child: Container(
+        height: size,
+        width: size,
+        decoration: BoxDecoration(
+          color: Theme.of(context).primaryColor.withOpacity(0.1),
+          shape: BoxShape.circle,
+        ),
       ),
     );
   }
