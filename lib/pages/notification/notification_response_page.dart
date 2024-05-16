ab9b77c56e4c08b6a4a9adb245d913333181e2f6
yash <yashlathar07@gmail.com>
2024-05-16 21:57:47 +0530

    notificatioResponsePage updated

--- lib/pages/notification/notification_response_page.dart
+++ lib/pages/notification/notification_response_page.dart
@@ -1,53 +1,237 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:secure_gates_project/controller/user_controller.dart';
+import 'package:secure_gates_project/widgets/circular_button.dart';
+import 'package:secure_gates_project/widgets/vertical_divider_widget.dart';
 
-class NotificationResponsePage extends StatelessWidget {
-  final String visitorName;
-  final String visitorImage;
+import '../../entities/visitor_from_notification.dart';
+
+class NotificationResponsePage extends HookConsumerWidget {
+  final VisitorFromNotification notificationVisitor;
+
   const NotificationResponsePage({
     super.key,
-    required this.visitorImage,
-    required this.visitorName,
+    required this.notificationVisitor,
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     final size = MediaQuery.of(context).size;
+    final socName = ref.read(userControllerProvider).currentUser!.socName;
 
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Notification Screen",
-          style: TextStyle(
-            color: Colors.white,
+      body: Column(
+        children: [
+          Container(
+            width: size.width,
+            height: size.height / 1.7,
+            padding: const EdgeInsets.only(
+              left: 15,
+              right: 15,
+              top: 30,
+            ),
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(50),
+                bottomRight: Radius.circular(50),
+              ),
+              gradient: LinearGradient(
+                  colors: [
+                    Color(0xFFf7625c),
+                    Color(0xFFffa8bc),
+                  ],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp),
+            ),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        context.pop();
+                      },
+                      icon: const Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  "Secure Gates",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 30),
+                ClipRRect(
+                  borderRadius: const BorderRadius.all(
+                    Radius.circular(
+                      90,
+                    ),
+                  ),
+                  child: Image(
+                    image: NetworkImage(
+                      notificationVisitor.visitorImage,
+                    ),
+                    height: 150,
+                    width: 150,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  notificationVisitor.visitorName,
+                  style: const TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      notificationVisitor.visitorType,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(width: 5),
+                    const VerticallyDivider(
+                      color: Colors.white,
+                    ),
+                    const SizedBox(width: 5),
+                    Text(
+                      "${notificationVisitor.visitorTypeDetail} is",
+                      style: const TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                const Text(
+                  "waiting at the door",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 25),
+                Transform.translate(
+                  offset: const Offset(0, 40),
+                  child: Container(
+                    height: 60,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      border: Border.all(
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        socName,
+                        style: const TextStyle(
+                          fontSize: 25,
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-      ),
-      body: SizedBox(
-        width: size.width,
-        height: size.height,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Center(
-              child: Text("Notifiation Received"),
+          const Padding(
+            padding: EdgeInsets.only(top: 80),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Column(
+                  children: [
+                    CircularButton(
+                      color: Colors.redAccent,
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Deny Visitor",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.redAccent,
+                      ),
+                    ),
+                  ],
+                ),
+                Column(
+                  children: [
+                    CircularButton(
+                      color: Colors.lightGreen,
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Accept Visitor",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.lightGreen,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Center(
-              child: Text(visitorName),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    CircularButton(
+                      color: Colors.orangeAccent,
+                      child: Icon(
+                        Icons.check,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Leave at the gate",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.orangeAccent,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            // Center(
-            //   child: Image.network(visitorImage),
-            // ),
-          ],
-        ),
+          )
+        ],
       ),
     );
   }
-}
-
-void onAccept() {
-  print("accepted");
-}
-
-void onDeny() {
-  print("denied");
 }
